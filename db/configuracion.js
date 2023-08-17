73bbc8bc068a786ad00df47e92488dddee11e91a
Daniel Ruiz <danuruiz7@hotmail.com>
2023-08-17 22:21:27 +0200

    deplo2

--- db/configuracion.js
+++ db/configuracion.js
@@ -3,12 +3,20 @@ const pgp = require('pg-promise')(initOptions);
 
 // postgres://danudev:AaosDGBrS1m8b7igk56OJvfdLPPHvmpo@dpg-cjf7r1anip6c73ftl79g-a.frankfurt-postgres.render.com/dbmetasapp
 
+// const cn = {
+//   user: 'postgres',
+//   password: '061092',
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'metasapp',
+// };
+
 const cn = {
-  user: 'postgres',
-  password: '061092',
-  host: 'localhost',
+  user: 'danudev',
+  password: 'AaosDGBrS1m8b7igk56OJvfdLPPHvmpo',
+  host: 'dpg-cjf7r1anip6c73ftl79g-a',
   port: 5432,
-  database: 'metasapp',
+  database: 'dbmetasapp',
 };
 
 const db = pgp(cn);
