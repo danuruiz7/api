0eefd0ad754e1fe25e50c24df0b16ef653e2f204
Daniel Ruiz <danuruiz7@hotmail.com>
2023-08-17 22:15:21 +0200

    deplo

--- db/configuracion.js
+++ db/configuracion.js
@@ -1,6 +1,8 @@
 const initOptions = {};
 const pgp = require('pg-promise')(initOptions);
 
+// postgres://danudev:AaosDGBrS1m8b7igk56OJvfdLPPHvmpo@dpg-cjf7r1anip6c73ftl79g-a.frankfurt-postgres.render.com/dbmetasapp
+
 const cn = {
   user: 'postgres',
   password: '061092',
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('Bienvenido a la API de metas');
+  res.send('Bienvenido a la API de metas de DANIEL RUIZ');
 });
 
 module.exports = router;
